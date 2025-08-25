575ce6bcbcb5b9d8ea17eca3e126cdb6d81c72b4
ail1020 <ail1020@users.noreply.github.com>
2025-08-25 02:41:40 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 7
-        versionName = "1.3.3"
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
