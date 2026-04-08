bdcc9f5a2ec3a126994d63d1605d4bbd023656ca
ail1020 <ail1020@users.noreply.github.com>
2026-04-08 13:01:28 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
+        jvmToolchain(11)
     }
     buildFeatures {
         viewBinding = true
