32fae9f134685e50dc385cce7e564b361885263c
ail1020 <root@localhost>
2026-04-08 19:17:42 -0500

    added app files

--- app/src/main/java/info/ails/myapps/AppInfo.kt
+++
@@ -1,9 +0,0 @@
-package info.ails.myapps
-
-data class AppInfo(
-    val name: String,        // App name
-    val lastUpdate: Long,     // Last updated date (in milliseconds)
-    val firstInstallTime: Long, // First install date (in milliseconds)
-    val installer: String,   // Package name of the installer
-    val flags: Int = 0
-)
